4cd8e31b93549374c692bb4bbc02c69be58c2960
heeheepark <pjh9641@gmail.com>
2026-08-27 04:58:34 +0200

    chore: 프로젝트 기본 셋팅

---
+++ src/main/java/org/example/jubjubapi/JubjubApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jubjubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubjubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JubjubApiApplication.class, args);
+    }
+
+}
