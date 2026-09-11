578442ca5a1bb10e391a9f1805f639071a2ff286
heeheepark <pjh9641@gmail.com>
2026-09-11 17:04:55 +0200

    feat: Seat Entity에 performance_id 추가

--- src/main/java/org/example/jubjubapi/seat/entity/Seat.java
+++ src/main/java/org/example/jubjubapi/seat/entity/Seat.java
@@ -31,7 +31,8 @@ public class Seat {
     @Column(nullable = false)
     private Integer seatNumber;
 
-    public Seat(Long externalSeatId, String section, String seatRow, Integer seatNumber) {
+    public Seat(Performance performance, Long externalSeatId, String section, String seatRow, Integer seatNumber) {
+        this.performance = performance;
         this.externalSeatId = externalSeatId;
         this.section = section;
         this.seatRow = seatRow;
