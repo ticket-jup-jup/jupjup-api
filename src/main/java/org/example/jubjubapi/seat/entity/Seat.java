53b0435972d80436c55481fb5dc142551bd9e342
heeheepark <pjh9641@gmail.com>
2026-09-11 16:35:13 +0200

    feat: Seat Entity 추가

---
+++ src/main/java/org/example/jubjubapi/seat/entity/Seat.java
@@ -0,0 +1,32 @@
+package org.example.jubjubapi.seat.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "seats")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1)
+    private String section;
+
+    @Column(nullable = false, length = 2)
+    private String seatRow;
+
+    @Column(nullable = false)
+    private Integer seatNumber;
+
+    public Seat(String section, String seatRow, Integer seatNumber) {
+        this.section = section;
+        this.seatRow = seatRow;
+        this.seatNumber = seatNumber;
+    }
+}
